6894f29656ebd33aec015e515f206a69e27a94ca
Илья Самойлов <141013914+PressureFlow@users.noreply.github.com>
2023-11-13 22:42:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function squareOfSum (a,b){
+    console.log((a ** 2) + (2 * a * b) + (b ** 2) );
+}
+
+squareOfSum (8, 5);
+
+function squareOfSum (a,b){
+    console.log((a ** 2) + (2 * a * b) + (b ** 2) );
+}
+
+squareOfSum (9, 6);
